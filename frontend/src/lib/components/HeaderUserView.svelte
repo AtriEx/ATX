5f50ca10fc25107dfc99ae0c27f917a29e7ca87c
sakulstar <111194775+sakulstar@users.noreply.github.com>
2024-02-03 13:38:27 +0100

    Changed dropdown menu for mobile

--- frontend/src/lib/components/HeaderUserView.svelte
+++ frontend/src/lib/components/HeaderUserView.svelte
@@ -20,18 +20,35 @@
         <CurrencyView />
         <NetworthView />
     </div>
-    <button class="flex flex-row items-center" on:click={toggleDropdown}> <!-- This is the anchor that can open the dropdown.-->
+    <button class="max-sm:hidden flex flex-row items-center" on:click={toggleDropdown}> <!-- This is the anchor that can open the dropdown.-->
         <div class="flex flex-row items-center space-x-2">
             <span>{$username}</span>
             <img src={$userPfp} class="rounded-full border-solid border-2 border-white object-fill h-10" alt="The user's twitch profile."/>
         </div>
     </button>
+    <button on:click={toggleDropdown} type="button" class="sm:hidden text-gray-500 hover:text-white focus:text-white focus:outline-none">
+        <svg class="h-8 w-8 fill-current" viewBox="0 0 24 24">
+            <path class="{isDropdownVisible == true ? "block" : "hidden"}" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
+            <path class="{isDropdownVisible == true ? "hidden" : "block"}" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
+        </svg>
+    </button>
     {#if isDropdownVisible}
-    <div class="absolute top-16 right-0 bg-blue-950 rounded-b-lg drop-shadow-lg" transition:slide={{ delay: 0, duration: 300}}>
+    <div class="max-sm:hidden absolute top-16 right-0 bg-blue-950 rounded-b-lg drop-shadow-lg" transition:slide={{ delay: 0, duration: 300}}>
         <div class="flex flex-col items-start">
             <a class="hover:text-white hover:bg-slate-800 cursor-pointer w-full" href="/profile/{$username}"><div class="m-2">Profile</div></a>
             <a class="hover:text-white hover:bg-slate-800 cursor-pointer w-full" href="/settings"><div class="m-2">Account Settings</div></a>
             <div class="hover:text-white bg-slate-900 hover:bg-slate-950 w-full p-2 rounded-b-lg cursor-pointer"><a  href="">Sign Out</a></div>
+        </div>
+    </div>
+    <div class="sm:hidden absolute top-16 right-0 px-4 py-5 border-t bg-blue-950 border-gray-800" transition:slide={{ delay: 0, duration: 300}}>
+        <div class="flex items-center">
+            <img class="h-8 w-8 rounded-full border-2 border-gray-600 object-cover" src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80" alt="Your avatar">
+            <span class="ml-3 font-semibold text-white">Jane Doe</span>
+        </div>
+        <div class="mt-4">
+            <a href="/profile/{$username}" class="block text-gray-400 hover:text-white">Profile</a>
+            <a href="/settings" class="block text-gray-400 hover:text-white">Account settings</a>
+            <a href="#" class="mt-2 block text-gray-400 hover:text-white">Sign out</a>
         </div>
     </div>
     {/if}
