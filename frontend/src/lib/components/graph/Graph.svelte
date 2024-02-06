9ba831b619eb5d1d8083ee874fcbca625cd9dd12
Joshua Coombes-Hall <jtec77@gmail.com>
2024-02-06 16:02:28 +1100

    Initial blank Graph component.

---
+++ frontend/src/lib/components/graph/Graph.svelte
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+
+<div>
+    I'm a graph!
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+    import Graph from "$lib/components/graph/Graph.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Graph />
