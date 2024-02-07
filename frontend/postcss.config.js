0a0801ff3daae9ac0a2daf58988377685640d835
quabey <50772988+quabey@users.noreply.github.com>
2024-02-07 16:37:42 +0100

    Fix package issues

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,9 +1,6 @@
 export default {
 	plugins: {
 		'tailwindcss/nesting': 'postcss-nesting',
-		'postcss-preset-env': {
-			features: { 'nesting-rules': false }
-		},
 		autoprefixer: {},
 		tailwindcss: {}
 	}
