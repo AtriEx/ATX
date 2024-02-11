499d6aa2d5c1073acb3c40b753f97988845c3d73
sakulstar <111194775+sakulstar@users.noreply.github.com>
2024-02-11 19:51:41 +0100

    Added meet the team card page and UI

---
+++ frontend/src/lib/components/meetTheTeamCard.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import Fa from 'svelte-fa';
+    import {faTwitter, faGithub, faDiscord} from '@fortawesome/free-brands-svg-icons'
+
+    export let name: string;
+    export let description: string;
+    export let image: string;
+    export let role: string;
+    export let twitterUsername: string;
+    export let discordUserID: string;
+    export let githubUsername: string;
+</script>
+
+<div class="relative w-60 h-80 bg-light-accent/5 dark:bg-dark-accent/5 border-light-accent dark:border-dark-accent rounded-lg border-[1px]">
+    <div class="flex flex-col items-center">
+        <img class="w-20 h-20 rounded-3xl mt-5" src={image} alt="contributor profile"/>
+        <p class="font-semibold text-2xl select-none text-light-text dark:text-dark-text">{name}</p>
+        <p class="font-medium text-xl select-none text-light-primary dark:text-dark-primary">{role}</p>
+        <p class="font-normal text-lg select-none px-3 text-light-text dark:text-dark-text">{description}</p>
+    </div>
+    <div class="mx-3 pt-1 absolute bottom-2 left-0 right-0 self-end border-t-[1px] flex flex-row justify-center border-light-accent dark:border-dark-accent {twitterUsername == null && githubUsername == null && discordUserID == null ? "mb-[21.333px]" : "block"}">
+        <a class="{twitterUsername == null ? "hidden" : "block"}"  href="https://twitter.com/{twitterUsername}" target="_blank"><Fa class="text-light-accent dark:text-dark-accent mx-1" size="lg" icon={faTwitter} /></a>
+        <a class="{githubUsername == null ? "hidden" : "block"}"  href="https://github.com/{githubUsername}" target="_blank"><Fa class="text-light-accent dark:text-dark-accent mx-1" size="lg" icon={faGithub} /></a>
+        <a class="{discordUserID == null ? "hidden" : "block"}"  href="https://discordlookup.com/user/{discordUserID}" target="_blank"><Fa class="text-light-accent dark:text-dark-accent mx-1" size="lg" icon={faDiscord} /></a>
+    </div>
+</div>
---
+++ frontend/src/routes/contributors/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import MeetTheTeamCard from '$lib/components/meetTheTeamCard.svelte';
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+
+    
+</script>
+
+<main class="mt-2 mb-6 max-sm:mb-24 flex flex-col items-center text-center">
+    <p class="text-6xl max-sm:text-[42px] font-semibold text-light-text dark:text-dark-text">Meet the <span class="font-bold text-light-accent dark:text-dark-accent">team.</span></p>
+    <div class="mt-8 mx-5 grid grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 grid-flow-row gap-8">
+        {#each data.data as contributor}
+            <MeetTheTeamCard name={contributor.name} description={contributor.description} image={contributor.image} role={contributor.role} twitterUsername={contributor.twitterUsername} discordUserID={contributor.discordUserID} githubUsername={contributor.githubUsername} />
+        {/each}
+    </div>
+</main>
