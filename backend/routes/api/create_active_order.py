047889f8a181e4d7f99c0fa2b70b282bbdcf58ed
Josh Steubs <joshua.steubs@gmail.com>
2024-02-24 02:40:17 -0800

    Add create_active_order function to create active buy/sell orders

--- backend/routes/api/create_active_order.py
+++ backend/routes/api/create_active_order.py
@@ -22,3 +22,16 @@ def create_sell_order():
     test_entry = test_data.test_entry_2()
     supabase_middleman.insert_entry('active_buy_sell', test_entry)
     return
+
+def create_active_order(user_id: str, buy_or_sell: str, stock_id, price: float, quantity: int, expirey: str):
+    # Insert new entry into active_buy_sell table
+    entry = {"userId": user_id,
+            "buy_or_sell": buy_or_sell,
+            "stockId": stock_id,
+            "price": price,
+            "quantity": quantity,
+            "time_posted": datetime.now().isoformat(),
+            "expirey": expirey
+            }
+    supabase_middleman.insert_entry('active_buy_sell', entry)
+    return
