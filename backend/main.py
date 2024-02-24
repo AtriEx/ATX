58311cb7066eb4cfa6bf088efc60507e041f31d2
sovamorco <sovamorco@sovamor.co>
2024-02-24 16:13:07 +0400

    fix linter complaints

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,21 @@
-from datetime import datetime, timedelta
-from fastapi import FastAPI
-# pylint: disable=no-name-in-module # it's looking in the supabase folder in project root
-from supabase import create_client, Client
-from database import supabase_middleman
-from routes import create_active_order
-from routes import buy_order
-from dotenv import load_dotenv
-import os
+"""API routes declaration"""
 
+from fastapi import FastAPI
+
+from routes import buy_order, create_active_order
 
 app = FastAPI()
 
 
-@app.get('/buyOrder')
+@app.get("/buyOrder")
 def test_entry_1():
+    """API route for creating a test buy order."""
     create_active_order.create_buy_order()
     return "Test entry inserted"
 
 
-@app.get('/qb')
+@app.get("/qb")
 def create_buy_order():
+    """API route for creating a buy order for one share of a stock."""
     buy_order.buy_order()
     return "Quick buy executed"
---
+++ backend/util/test_data.py
@@ -0,0 +1,41 @@
+"""Test data for testing transactions"""
+
+from datetime import datetime, timedelta
+
+
+# pylint: disable=R0801 # test data
+def test_entry_1() -> dict:
+    """
+    Creates a test buy order table entry and returns it
+    """
+    test_entry = {
+        "userId": "d8a7ae82-1704-41bf-96a3-9347a5c022c8",
+        "buy_or_sell": True,
+        "stockId": 2,
+        "price": 15,
+        "quantity": 1,
+        "time_posted": datetime.now().isoformat(),
+        "expirey": (
+            datetime.now() + timedelta(hours=1)
+        ).isoformat(),  # This is a test value; users will input an expiry date
+    }
+    return test_entry
+
+
+# pylint: disable=R0801 # test data
+def test_entry_2() -> dict:
+    """
+    Creates a test sell order table entry and returns it
+    """
+    test_entry = {
+        "userId": "572a902e-de7a-4739-adfe-f4af32a3f18b",
+        "buy_or_sell": False,
+        "stockId": 2,
+        "price": 15,
+        "quantity": 1,
+        "time_posted": datetime.now().isoformat(),
+        "expirey": (
+            datetime.now() + timedelta(hours=1)
+        ).isoformat(),  # This is a test value; users will input an expiry date
+    }
+    return test_entry
