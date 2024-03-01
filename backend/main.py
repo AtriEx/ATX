4da127f883a3c80335d789d14c1577cb5da1e96f
Josh Steubs <joshua.steubs@gmail.com>
2024-03-01 11:17:00 -0800

    Add createActiveOrder endpoint to main.py and add multiple entries for quant gt 0

--- backend/main.py
+++ backend/main.py
@@ -12,14 +12,39 @@ import os
 app = FastAPI()
 
 
-
-@app.get('/buyOrder')
+@app.get("/buyOrder")
 def test_entry_1():
     create_active_order.create_buy_order()
     return "Test entry inserted"
 
 
-@app.get('/qb')
+@app.get("/qb")
 def create_buy_order():
     buy_order.buy_order()
     return "Quick buy executed"
+
+
+@app.get("/createActiveOrder")
+def create_active_order(
+    buy_or_sell: bool,
+    price: int,
+    expirey: datetime,
+    quantity: int,
+    stockId: int,
+    userId: str,
+):
+    if price < 1:
+        return "Price must be greater than 0"
+    if quantity < 1:
+        return "Quantity must be greater than 0"
+    if expirey < datetime.now():
+        return "Expirey must be in the future"
+    if stockId < 1:  # TODO determine what is valid stockId
+        return "Invalid stockId"
+    if userId == "":  # TODO determine what is valid userId
+        return "Invalid userId"
+    time_posted = datetime.now().isoformat()
+    create_active_order.create_active_order(
+        time_posted, buy_or_sell, price, expirey, quantity, stockId, userId
+    )
+    return "Active order created"
--- backend/routes/api/create_active_order.py
+++ backend/routes/api/create_active_order.py
@@ -5,33 +5,65 @@ from dotenv import load_dotenv
 from datetime import datetime, timedelta
 from util.ext import test_data
 from database.supabase.store import supabase_middleman
-app=FastAPI()
-load_dotenv()
+from uuid import uuid4
 
+app = FastAPI()
+load_dotenv()
 
 
 #
 def create_buy_order():
     # Insert a test entry into the active_buy_sell table
     test_entry = test_data.test_entry_1()
-    supabase_middleman.insert_entry('active_buy_sell', test_entry)
-    return 
+    supabase_middleman.insert_entry("active_buy_sell", test_entry)
+    return
+
 
 def create_sell_order():
     # Insert a test entry into the active_buy_sell table
     test_entry = test_data.test_entry_2()
-    supabase_middleman.insert_entry('active_buy_sell', test_entry)
+    supabase_middleman.insert_entry("active_buy_sell", test_entry)
     return
 
-def create_active_order(user_id: str, buy_or_sell: bool, stock_id, price: float, quantity: int, expirey: str):
-    # Insert new entry into active_buy_sell table
-    entry = {"userId": user_id,
+
+"""
+active_buy_sell table schema:
+time_posted: timestamp
+buy_or_sell: bool
+price: int8
+expirey: timestamp
+quantity: int (this should always be 1. when we have an order submitted for 10, we break it up into 10 identical rows of quant 1)
+stockId: int8
+userId: uuid(str)
+orderId: uuid(str)
+"""
+
+
+# Creates entries to active_buy_sell table, adding one entry for each quantity
+def create_active_order(
+    time_posted: datetime,
+    buy_or_sell: bool,
+    price: int,
+    expirey: datetime,
+    quantity: int,
+    stockId: int,
+    userId: str,
+):
+    generated_order_id = str(uuid4())
+
+    # Generate an entry for each quantity and insert those entries into the table
+    for i in range(quantity):
+        entry = {
+            "time_posted": time_posted,
             "buy_or_sell": buy_or_sell,
-            "stockId": stock_id,
             "price": price,
-            "quantity": quantity,
-            "time_posted": datetime.now().isoformat(),
-            "expirey": expirey
-            }
-    supabase_middleman.insert_entry('active_buy_sell', entry)
+            "expirey": expirey,
+            "quantity": 1,
+            "stockId": stockId,
+            "userId": userId,
+            "orderId": generated_order_id,
+            "has_been_processed": False,
+        }
+        # Insert the entry into the active_buy_sell table
+        supabase_middleman.insert_entry("active_buy_sell", entry)
     return
