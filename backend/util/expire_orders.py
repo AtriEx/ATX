9875c69ec83c754f91b9a54d320102cd3cbbea71
FloncDev <FloncDev@users.noreply.github.com>
2024-03-13 15:55:00 +0000

    :sparkles: Expire orders

--- backend/util/expire_orders.py
+++ backend/util/expire_orders.py
@@ -4,34 +4,47 @@ has expired.
 """
 
 import asyncio
+import os
 from contextlib import asynccontextmanager
 from datetime import datetime
 
+from dotenv import find_dotenv, load_dotenv
 from fastapi import FastAPI
 
 from database import supabase_middleman
 
+load_dotenv(find_dotenv())
+
+SLEEP_TIME = int(os.getenv("EXPIRE_LOOP_DELAY", 10))  # Default to 10s
+
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
+    """Runs before and after the server starts and closes"""
     task = asyncio.create_task(run_loop())
-    yield
+    yield  # Anything after here runs when server is shutting down
     task.cancel()
 
 
 async def run_loop():
     """Infinetly runs a loop to expire orders."""
+
+    await asyncio.sleep(5)  # Allow server to start
+
     while True:
         orders = supabase_middleman.get_active()
 
         for order in orders:
-            expiry_time = datetime.fromisoformat(order["expirey"])
+            # expirey column is optional
+            try:
+                expiry_time = datetime.fromisoformat(order["expirey"])
+            except KeyError:
+                continue
 
             # Since it's sorted, all orders after are also in the future
             if expiry_time > datetime.now():
                 break
 
-            # TODO
-            # Make it inactive
+            await supabase_middleman.expire_order(order["Id"])
 
-        await asyncio.sleep(10)
+        await asyncio.sleep(SLEEP_TIME)
