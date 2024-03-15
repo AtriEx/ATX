67268c49a96e869a583a24d0c4f912882ef71e58
bodyshady <135336198+bodyshadyy@users.noreply.github.com>
2024-03-15 00:20:41 +0000

    test commit

--- backend/database/supabase_middleman.py
+++ backend/database/supabase_middleman.py
@@ -198,7 +198,7 @@ def log_unfulfilled_order(order_info: dict) -> None:
 
 def networth_calculator(user_id: str) -> int:
     """
-    Gets the user's networth from profile, portfolio, and active_buy_sell
+    Gets the user's networth from profile, portfolio, and active_buy_sell test
 
     Args:
         user_id (int): The id of the user whose net worth is requested
