32239d047bc37e771c92703d156d4173e08de425
Skumbl <janarvik01@gmail.com>
2024-03-24 16:11:16 -0400

    tweaked the login and reward button to match current style, as well as adding confetti to the rewards popup

--- frontend/src/lib/components/Login.svelte
+++ frontend/src/lib/components/Login.svelte
@@ -12,16 +12,19 @@
 
 <div class="absolute top-0 left-0 w-screen h-screen z-1 flex justify-center items-center">
 	<div
-		class=" bg-light-primary h-3/8 md:h-1/5 dark:bg-dark-primary/50 lg:w-1/3 md:w-2/4 w-4/5 rounded-lg relative flex"
+		class=" bg-light-background dark:bg-dark-primary/5 h-3/8 md:h-1/5  lg:w-1/3 md:w-2/4 w-4/5 rounded-xl relative flex border border-dark-accent"
 	>
 		<div class="absolute scale-50 right-0 mr-1 text-light-text dark:text-dark-text">
 			<button on:click={() => ($loginDialog = false)}>
 				<Fa icon={faXmark} class="text-5xl" />
 			</button>
 		</div>
-		<div class="flex flex-col justify-between items-center w-full grow pt-5 pb-2">
-			<span class="text-light-text dark:text-dark-text text-3xl pt-2 font-extrabold">
-				Login with Twitch
+		<div class="flex flex-col justify-between items-center w-full grow pt-5 pb-2 px-6">
+
+			<span class=" flex flex-col items-center w-full border-b-2 border-dark-secondary text-light-text dark:text-dark-text ">
+
+				<h1 class="text-3xl pt-2 font-extrabold pb-2">Login with Twitch</h1>
+
 			</span>
 			<div class="p-2">
 				<button
--- frontend/src/lib/components/RewardsPopup.svelte
+++ frontend/src/lib/components/RewardsPopup.svelte
@@ -3,28 +3,29 @@
 	import { rewardsDialog } from  '$lib/stores/uiStates';
 	import { claimReward } from'$lib/supabase';
 	import { loggedIn, user, profile } from '../stores/userData';
+	import { Confetti } from "svelte-confetti"
 
 	const handleReward = () => {
 		claimReward();
 	};
+	
 </script>
 
 <div class="absolute top-0 left-0 w-screen h-screen z-1 flex justify-center items-center">
-	<div
-		class=" bg-light-background dark:bg-dark-secondary/20 h-3/8 md:h-1/5  lg:w-1/3 md:w-2/4 w-4/5 rounded-xl relative flex border-2 border-dark-accent"
-	>
-		<div class="flex flex-col justify-between items-center w-full grow pt-5 pb-2">
 
-			<span class=" flex flex-col items-center w-full border-b-2 border-dark-secondary text-light-text dark:text-dark-text ">
+	<div
+		class=" bg-light-background dark:bg-dark-primary/5 h-3/8 md:h-1/5  lg:w-1/3 md:w-2/4 w-4/5 rounded-xl relative flex border border-dark-accent"
+	>
+
+		<div class="flex flex-col justify-between items-center w-full grow pt-5 pb-2 px-6">
+			<span class=" flex flex-col items-center w-full px-4 border-b-2 border-dark-secondary text-light-text dark:text-dark-text ">
 
 				<h1 class="text-3xl pt-2 font-extrabold pb-2">
 					Daily Login Reward
 				</h1>
-
-				<p>*this is a dark pattern to keep you trading*</p>
-
 			</span>
-
+			<p class="text-light-text dark:text-dark-text">*this is a dark pattern to keep you trading*</p>
+			<!-- STOCK OR MONEY VALUES GOES IN THIS DIV -->
 			<div class="text-green-500">
 				&lt;Money or Stock PlaceHolder&gt;
 			</div>
@@ -34,8 +35,10 @@
 					class="bg-dark-primary text-light-text text-lg px-8 p-2 rounded-md flex flex-row items-center md:scale-125 mb-2 hover:bg-dark-primary/70"
 					on:click={handleReward}
 				>
+					<Confetti x={[-3, 3]} y={[-3,3]}/>
 					Claim
 				</button>
+
 			</div>
 		</div>
 	</div>
