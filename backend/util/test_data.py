1a527cd9f2d8c8926b38daa14bb7ccdef3a54fec
DatPieceOfCake12 <81319757+DatPieceOfCake12@users.noreply.github.com>
2024-03-27 10:53:59 -0500

    Adjusted test data script to allow it to be customizable

--- backend/util/test_data.py
+++ backend/util/test_data.py
@@ -6,16 +6,16 @@ from uuid import uuid4 as generate_order_id
 # pylint: disable=R0801 # test data
 
 
-def test_entry_1() -> dict:
+def buy_entry(data: dict) -> dict:
     """
     Creates a test buy order table entry and returns it
     """
     test_entry = {
         "userId": "d8a7ae82-1704-41bf-96a3-9347a5c022c8",
         "buy_or_sell": True,
-        "stockId": 2,
-        "price": 115,
-        "quantity": 1,
+        "stockId": data["stockId"],
+        "price": data["price"],
+        "quantity": data["quantity"],
         "time_posted": datetime.now().isoformat(),
         "orderId": str(generate_order_id()),
         "expirey": (
@@ -27,16 +27,16 @@ def test_entry_1() -> dict:
 
 
 # pylint: disable=R0801 # test data
-def test_entry_2() -> dict:
+def sell_entry(data: dict) -> dict:
     """
     Creates a test sell order table entry and returns it
     """
     test_entry = {
         "userId": "572a902e-de7a-4739-adfe-f4af32a3f18b",
         "buy_or_sell": False,
-        "stockId": 2,
-        "price": 150,
-        "quantity": 1,
+        "stockId": data["stockId"],
+        "price": data["price"],
+        "quantity": data["quantity"],
         "orderId": str(generate_order_id()),
         "time_posted": datetime.now().isoformat(),
         "expirey": (
