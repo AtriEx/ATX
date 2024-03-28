6bfe4927d8c242537dd2b7f22d64534d0c21d621
evanFroelich <evanfroe1234@yahoo.com>
2024-03-28 14:54:01 -0700

    prelim merging steubs changes into this branch

--- backend/main.py
+++ backend/main.py
@@ -8,41 +8,41 @@ from util.expire_orders import lifespan
 
 app = FastAPI(lifespan=lifespan)
 
-
 @app.get("/health")
 def health():
     """API route for health checks."""
     return ""
 
 
+
 @app.get("/buyOrder")
 def test_entry_1():
     """API route for creating a test buy order."""
-    create_active_order.create_sell_order()
+    #create_active_order.create_sell_order()
     return "Test entry inserted"
 
-
+#prod
 @app.get("/qb")
 def create_buy_order(data: dict):
     """API route for creating a buy order for one share of a stock."""
     ret_val = buy_order.buy_order(data)
     return ret_val
 
-
+#testing
 @app.get("/testParams")
 def test_params(data: dict):
     """API route for testing parameters."""
     buy_order.test_params()
     return data
 
-
+#testing
 @app.get("/insertCustomOrder")
 def insert_custom_order(data: dict):
     """API route for inserting a custom order."""
     supabase_middleman.insert_entry("active_buy_sell", data)
     return "Custom order inserted"
 
-
+#testing
 @app.get("/changeBalanceTest")
 def change_balance_test(amount: int):
     """API route for testing balance changes."""
@@ -51,9 +51,16 @@ def change_balance_test(amount: int):
     )
     return output
 
-
+#prod
 @app.get("/netWorth")
 def fetch_net_worth(user_id: str):
     """API route for calculating the net worth of a user."""
     net_worth = net_worth_calculator.net_worth_calculator(user_id)
     return net_worth
+
+#prod
+@app.get("/createActiveOrder")
+def create_order(data: dict):
+    """API route for creating an active order."""
+    create_active_order.create_active_buy_sell_order(data)
+    return "Active order created"
--- backend/routes/create_active_order.py
+++ backend/routes/create_active_order.py
@@ -1,24 +1,144 @@
-"""Helper functions to create test orders"""
+"""Creates buy and sell orders"""
 
+from fastapi import FastAPI, HTTPException
+from datetime import datetime
+from pydantic import BaseModel
 from database import supabase_middleman
-from util import test_data
+from uuid import UUID, uuid4
+from starlette.status import HTTP_400_BAD_REQUEST
+
+class ErrorResponse(BaseModel):
+    error_code: int
+    error_message: str
+    details: str = None  # Optional field
 
 
-def create_buy_order():
-    """
-    Inserst a test buy entry into active_buy_sell table.
 
-    Returns: None
-    """
-    test_entry = test_data.test_entry_1()
-    supabase_middleman.insert_entry("active_buy_sell", test_entry)
+def create_active_buy_sell_order(data: dict) -> str:
+    """Create a buy or sell order for a stock."""
+    buy_or_sell = data["buy_or_sell"]
+    price = data["price"]
+    expirey = data["expirey"]
+    quantity = data["quantity"]
+    stock_id = data["stockId"]
+    user_id = data["userId"]
 
 
-def create_sell_order():
-    """
-    Inserts a test sell entry into active_buy_sell table.
+    if price < 1:
+        raise_http_exception(
+            status_code=HTTP_400_BAD_REQUEST,
+            error_code=400,
+            error_message="Price must be greater than 0",
+        )
+    if quantity < 1:
+        raise_http_exception(
+            status_code=HTTP_400_BAD_REQUEST,
+            error_code=400,
+            error_message="Quantity must be greater than 0",
+        )
+    if expirey < datetime.now():
+        raise_http_exception(
+            status_code=HTTP_400_BAD_REQUEST,
+            error_code=400,
+            error_message="Expirey date cannot be in the past",
+        )
 
-    Returns: None
-    """
-    test_entry = test_data.test_entry_2()
-    supabase_middleman.insert_entry("active_buy_sell", test_entry)
+    if not supabase_middleman.fetch_stock_price(stock_id):
+        raise_http_exception(
+            status_code=HTTP_400_BAD_REQUEST,
+            error_code=400,
+            error_message="Stock not found",
+        )
+
+    # validate user_id is a valid uuid and it exists
+    try:
+        UUID(hex=user_id)  # throws value error if not a valid uuid
+        user_profile = supabase_middleman.fetch_profile(user_id)
+        if not user_profile:
+            raise_http_exception(
+                status_code=HTTP_400_BAD_REQUEST,
+                error_code=400,
+                error_message="User not found",
+            )
+    except ValueError:
+        raise_http_exception(
+            status_code=HTTP_400_BAD_REQUEST,
+            error_code=400,
+            error_message="user_id is not in valid UUID format",
+        )
+
+    # validate that if order is a buy order, user has enough balance, or if order is a sell order, user has enough quantity
+    if buy_or_sell:  # Buy order
+        user_profile = supabase_middleman.fetch_profile(user_id)
+        if not user_profile:
+            raise_http_exception(
+                status_code=HTTP_400_BAD_REQUEST,
+                error_code=400,
+                error_message="User not found",
+            )
+        if user_profile["balance"] < price * quantity:
+            raise_http_exception(
+                status_code=HTTP_400_BAD_REQUEST,
+                error_code=400,
+                error_message="Insufficient balance",
+            )
+    else:  # Sell order
+        portfolio = supabase_middleman.fetch_portfolio(user_id, stock_id)
+        if not portfolio:
+            raise_http_exception(
+                status_code=HTTP_400_BAD_REQUEST,
+                error_code=400,
+                error_message="Portfolio not found",
+            )
+        if portfolio["quantity"] < 1:
+            raise_http_exception(
+                status_code=HTTP_400_BAD_REQUEST,
+                error_code=400,
+                error_message="User does not own stock",
+            )
+        if portfolio["quantity"] < quantity:
+            raise_http_exception(
+                status_code=HTTP_400_BAD_REQUEST,
+                error_code=400,
+                error_message="Insufficient quantity",
+            )
+
+    time_posted = datetime.now().isoformat()
+    # create_active_order.create_active_order(
+    #     time_posted, buy_or_sell, price, expirey, quantity, stock_id, user_id
+    # )
+
+    generated_order_id = str(uuid4())
+
+    if buy_or_sell:
+        supabase_middleman.update_user_balance(user_id, price * quantity * -1)
+        #supabase_middleman.escrow_funds(user_id, price, quantity)
+    else:
+        supabase_middleman.update_user_portfolio(user_id, stock_id, quantity * -1)
+        #supabase_middleman.escrow_stock(user_id, stock_id, quantity)
+
+    # Generate an entry for each quantity and insert those entries into the table
+    for i in range(quantity):
+        entry = {
+            "time_posted": time_posted,
+            "buy_or_sell": buy_or_sell,
+            "price": price,
+            "expirey": expirey,
+            "quantity": 1,
+            "stockId": stock_id,
+            "userId": user_id,
+            "orderId": generated_order_id,
+            "has_been_processed": False,
+        }
+        # Insert the entry into the active_buy_sell table
+        supabase_middleman.insert_entry("active_buy_sell", entry)
+    return "Active order created"
+
+
+def raise_http_exception(status_code, error_code, error_message, details=None):
+    error_response = ErrorResponse(
+        error_code=error_code, error_message=error_message, details=details
+    )
+    raise HTTPException(
+        status_code=status_code, detail=error_response.model_dump_json()
+    )
